b4cf799d018c8c1de755bf17b413f79feade8336
jack <biffisbuffm@gmail.com>
2020-06-25 14:22:09 -0500

    errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,27 +62,19 @@ $(".list-group").on("click", "p", function() {
 $(".list-group").on("blur", "textarea", function() {
   // get the textarea's current value/text
   var text = $(this)
-    .val()
-    .trim();
+  .val()
+  .trim();
 
   var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
 
   var index = $(this)
-    .closest(".list-group-item")
-    .index();
-
-  tasks[status][index].text = text;
-  saveTasks();
-
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(text);
-
+  .closest(".list-group-item");
+  
+  var taskP = $('<p>').addClass("m-1").text(text);
   $(this).replaceWith(taskP);
-
 });
 
 // due date was clicked
@@ -186,4 +178,53 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {// array to store the task data in
+    var tempArr = [];
+    
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+    
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+    
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    
+    // trim down lists id to match ob property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
 
+      //update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+    
+}
+});
