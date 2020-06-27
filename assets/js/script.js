0ed5db525f97c38dc6ea924f5c049d637dea6dd8
jack <biffisbuffm@gmail.com>
2020-06-27 10:51:45 -0500

    edits time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,20 @@ var tasks = {};
 
 var auditTask = function(taskEl) {
   // get date from task element
-  var date = $(taskEl).find("span").text().trim();
+  var date = taskEl.find("span").text().trim();
 
   //convert to moment obect at 5:00pm
   var time = moment(date, "L").set("hour", 17);
  
   // remove any old classes from element
-  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  taskEl.removeClass("list-group-item-warning list-group-item-danger");
 
   // apply new class if task is near/over due date
   if (moment().isAfter(time)) {
-    $(taskEl).addClass("list-group-item-danger");
+    taskEl.addClass("list-group-item-danger");
   }
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
-    $(taskEl).addClass("list-group-item-warning");
+    taskEl.addClass("list-group-item-warning");
   }
 
   console.log(taskEl);
@@ -268,8 +268,8 @@ $("#modalDueDate").datepicker({
 });
 
 setInterval(function () {
- $(".card .list-group-item").each(function (el) {
-   auditTask(el);
+ $(".card .list-group-item").each(function () {
+   auditTask($(this));
    
  });
  
