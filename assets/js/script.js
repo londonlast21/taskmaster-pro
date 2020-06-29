c9f4447d5b46878f37da1d04d1fd59268a684803
jack <biffisbuffm@gmail.com>
2020-06-29 15:21:35 -0500

    trash

--- assets/js/script.js
+++ assets/js/script.js
@@ -204,16 +204,20 @@ $(".card .list-group").sortable({
 tolerance: "pointer",
  helper: "clone",
  activate: function(event) {
-   console.log("activate", this);
+   $(this).addClass("dropover");
+   $("bottom-trash").addClass("bottom-trash-drag");
  },
  deactivate: function(event) {
-   console.log("deactivate", this);
+   $(this).removeClass("dropover");
+   $("bottom-trash").removeClass("bottom-trash-drag");
  },
  over: function(event) {
-   console.log("over", event.target);
+
+   $(event.target).addClass("dropover-active");
+
  },
  out: function(event) {
-   console.log("out", event.target);
+   $(event.target).removeClass("dropover-active");
  },
  update: function(event) {// array to store the task data in
    var tempArr = [];
@@ -250,15 +254,18 @@ tolerance: "pointer",
 $("#trash").droppable({
  accept: ".card .list-group-item",
  tolerance: "touch",
+ 
  drop: function(event, ui) {
    ui.draggable.remove();
    console.log("drop");
  },
  over: function(event, ui) {
    console.log("over");
+   $("bottom-trash").addClass("bottom-trash-active");
  },
  out: function(event, ui) {
    console.log("out");
+   $("bottom-trash").removeClass("bottom-trash-active")
  }
 });
 
